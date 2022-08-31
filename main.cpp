ebb12806207feefe85b6097a1e0b2e2572ad0d6f
Ian <ianvzs@outlook.com>
2022-08-31 22:48:46 +0800

    go~

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#include <QDebug>
+#include <QLocale>
+#include <QTranslator>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "QCV_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            app.installTranslator(&translator);
+            break;
+        }
+    }
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    qDebug() << "RGB2BGR int is: " << cv::COLOR_RGB2BGR;
+
+    return app.exec();
+}
